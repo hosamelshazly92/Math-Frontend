15980677b7518d9e00b650d4a9e60656689c52ef
hosamelshazly92 <hosamelshazly92@gmail.com>
2022-02-26 13:59:28 +0200

    add loading spinner

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import { useEffect, useState } from "react";
 // components
 import Question from "./components/Question/Question";
 import Answers from "./components/Answers/Answers";
+import Tag from "./components/Tag/Tag";
+
+// imagery
+import spinner from "./assets/gif/loading-spinner.gif";
 
 // styles
 import styles from "./App.module.css";
+
+// vars
+const { appContainer, loadingContainer, loadingSpinner, loadingText } = styles;
 
 function App() {
   const [questions, setQuestions] = useState([]);
@@ -15,19 +22,23 @@ function App() {
       .then(async (res) => {
         const data = await res.json();
         console.log(data.questions);
-        setQuestions(data.questions);
+        // setQuestions(data.questions);
       })
       .catch((err) => console.log(err));
   }, []);
 
   return (
-    <div>
+    <div className={appContainer}>
       {questions.length === 0 ? (
-        <p>loading...</p>
+        <div className={loadingContainer}>
+          <img className={loadingSpinner} src={spinner} alt="loading spinner" />
+          <p className={loadingText}>loading</p>
+        </div>
       ) : (
         <div>
           {questions.map((itm) => (
             <div key={itm._id}>
+              <Tag>{itm.type}</Tag>
               <Question>{itm.question}</Question>
               <Answers data={itm.answers} />
             </div>
---
+++ src/components/Tag/Tag.js
@@ -0,0 +1,9 @@
+const Tag = ({ children }) => {
+  return (
+    <div>
+      <p>{children}</p>
+    </div>
+  );
+};
+
+export default Tag;
